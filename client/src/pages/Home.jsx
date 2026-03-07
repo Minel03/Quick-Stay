c95e83599d8b4b411324b9c3e40b93a72be5ef2e
Minel03 <saint.mynel@gmail.com>
2026-03-07 11:59:12 +0800

    1:53:05 making testimonial page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import FeaturedDestination from '../components/FeaturedDestination';
+import ExlusiveOffers from '../components/ExlusiveOffers';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
+      <ExlusiveOffers />
     </>
   );
 };
