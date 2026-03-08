a759e6d1f08bbcdd6499c21b6d4f412344014add
Minel03 <saint.mynel@gmail.com>
2026-03-08 22:20:00 +0800

    2:16:40 making hotel rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -17,6 +18,7 @@ const App = () => {
           />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 };
