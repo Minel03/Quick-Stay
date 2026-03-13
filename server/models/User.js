979c9639fef05a34b3bd9378f6cba89d21e28bcf
Minel03 <saint.mynel@gmail.com>
2026-03-13 11:52:10 +0800

    another fix

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     _id: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
-    name: { type: String, required: true },
     image: { type: String, required: true },
     role: { type: String, enum: ['user', 'hotelOwner'], default: 'user' },
     recentSearchedCities: [{ type: String, required: true }],
