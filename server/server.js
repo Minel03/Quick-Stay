c08c3ae2d1fdc1deee437efb55eb89fc5ce9bafd
Minel03 <saint.mynel@gmail.com>
2026-03-13 11:25:36 +0800

    5:54:40 deploying on vercel

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './config/mongodb.js';
+import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from './controllers/clerkWebhooks.js';
 
 connectDB();
 
 const app = express();
 app.use(cors()); // Enable Cros-Origin-Resource-Sharing
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(clerkMiddleware());
+
+// API to listen to clerk webhooks
+app.use('/api/clerk', clerkWebhooks);
 
 app.get('/', (req, res) => {
   res.send('API is Working');
